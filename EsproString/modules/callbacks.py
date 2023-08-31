d413e861c33d84b240993e4ce22e8e3ba031f5d4
Espro Music <143268188+EsproMusic@users.noreply.github.com>
2023-08-31 19:46:05 +0530

    Espro String

--- EsproString/modules/callbacks.py
+++ EsproString/modules/callbacks.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import CallbackQuery
 
-from StringGen import Anony
-from StringGen.utils import gen_key
-from StringGen.modules.gen import gen_session
+from EsproString import Anony
+from EsproString.utils import gen_key
+from EsproString.modules.gen import gen_session
 
 
 @Anony.on_callback_query(

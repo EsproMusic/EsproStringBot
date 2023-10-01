e0e953b26334a6ede3a7b4df08fa4872b940a93b
Espro Music <143268188+EsproMusic@users.noreply.github.com>
2023-10-01 22:13:28 +0530

    Espro String

--- EsproString/utils/inline.py
+++ EsproString/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/l_ll_ritikraj_ll_l"
+                text="ᴏᴡɴᴇʀ", url="https://t.me/i_ii_ritikraj_ii_i"
             ),
         ],
     ]
